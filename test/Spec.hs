676f8c5e826e15d6a3d04f69f0ad2b129c3e3ce8
StanleyStanMarsh <iastafev56@gmail.com>
2024-12-01 20:35:29 +0300

    feat: test addition by modulo prop

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
+import Lib
+import Test.QuickCheck
+
+-- без Positive - исключение деление на 0 (даже на `mod`)
+prop_withBuiltinAddMod x y (Positive m) = (addMod x y m) `mod` m == (x + y) `mod` m
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    quickCheck prop_withBuiltinAddMod
+    putStrLn "OK"
