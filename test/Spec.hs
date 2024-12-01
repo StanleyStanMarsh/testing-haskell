ef98384b1ab3a525d6f75b0dbc01901b53bff0bc
StanleyStanMarsh <iastafev56@gmail.com>
2024-12-01 20:39:59 +0300

    feat: test neutral element prop

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,9 +2,15 @@ import Lib
 import Test.QuickCheck
 
 -- без Positive - исключение деление на 0 (даже на `mod`)
+prop_withBuiltinAddMod :: Int -> Int -> Positive Int -> Bool
 prop_withBuiltinAddMod x y (Positive m) = (addMod x y m) `mod` m == (x + y) `mod` m
+
+prop_neutralElement :: Int -> Positive Int -> Bool
+prop_neutralElement x (Positive m) = addMod x 0 m == x `mod` m
 
 main :: IO ()
 main = do
     quickCheck prop_withBuiltinAddMod
-    putStrLn "OK"
+
+    quickCheck prop_neutralElement
+    putStrLn "ALL TESTS ARE PASSED"
